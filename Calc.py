44651defa46533bf72f9eecca04b587df8ea5518
PTomasz05 <161390435+PTomasz05@users.noreply.github.com>
2026-03-17 03:06:35 +0100

    Fix code visibility

--- Calc.py
+++ Calc.py
@@ -1,13 +1,17 @@
 while True:
     print("\nWitaj w kalkulatorze, wybierz dzialanie z podanych poniżej:")
     print("1. Dodawanie\n2. Odejmowanie\n3. Mnożenie\n4. Dzielenie")
+
     wybrane_dzialanie = input()
     if wybrane_dzialanie not in {"1", "2", "3", "4"}:
         print("Nie ma takiego dzialania!")
         continue
+
     print("Wybrales dzialanie:", wybrane_dzialanie)
+
     liczba_a = float(input("Podaj liczbę a: "))
     liczba_b = float(input("Podaj liczbę b: "))
+
     if wybrane_dzialanie == "1":
         print("Wynik dodawania:", liczba_a + liczba_b)
     elif wybrane_dzialanie == "2":
@@ -19,6 +23,8 @@ while True:
             print("Nie można dzielić przez zero!")
         else:
             print("Wynik dzielenia:", liczba_a / liczba_b)
+
     kolejne = input("Czy chcesz wykonać kolejne działanie? (tak/nie): ")
     if kolejne == "nie":
         break
+
